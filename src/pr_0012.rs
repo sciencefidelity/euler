3c5a2624dc86a79535e6b53b7169a2b65b8d9e82
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-08-12 16:57:36 +0100

    refactor: optimise 12

--- src/pr_0012.rs
+++ src/pr_0012.rs
@@ -1,33 +1,41 @@
 pub struct Euler;
 
 impl Euler {
-    pub fn highly_divisible_triangular_number(n: usize) -> usize {
-        let (mut t, mut a, mut cnt) = (1, 1, 0);
-        let prime_array = Self::generate_prime_array(65500);
-        while cnt <= n {
-            cnt = 1;
-            a += 1;
-            t += a;
-            let mut tt = t;
-            for prime in prime_array.iter().take(n) {
-                if prime * prime > tt {
-                    cnt *= 2;
+    pub fn highly_divisible_triangular_number(p: usize) -> usize {
+        let (mut n, mut dn, mut cnt) = (3, 2, 0);
+        let prime_array = Self::generate_prime_array(p / 2);
+        let mut max_i = 0;
+        while cnt <= p {
+            n += 1;
+            let mut n1 = n;
+            if n1 % 2 == 0 {
+                n1 /= 2;
+            }
+            let mut dn1 = 1;
+            for i in 0..p {
+                max_i = max_i.max(i);
+                if prime_array[i].pow(2) > n1 {
+                    dn1 *= 2;
                     break;
                 }
                 let mut exponent = 1;
-                while tt % prime == 0 {
+                while n1 % prime_array[i] == 0 {
                     exponent += 1;
-                    tt /= prime;
+                    n1 /= prime_array[i];
                 }
                 if exponent > 1 {
-                    cnt *= exponent;
+                    dn1 *= exponent;
                 }
-                if tt == 1 {
+                if n1 == 1 {
                     break;
                 }
             }
+            cnt = dn * dn1;
+            dn = dn1;
         }
-        t
+        println!("{}", prime_array.len());
+        println!("{max_i}");
+        n * (n - 1) / 2
     }
 
     fn generate_prime_array(mut n: usize) -> Vec<usize> {
@@ -39,7 +47,7 @@ impl Euler {
             if Self::is_prime(i32::try_from(candidate).unwrap()) {
                 primes.push(candidate);
             }
-            if candidate == n {
+            if candidate > n {
                 return primes;
             }
         }
@@ -86,4 +94,14 @@ mod tests {
     fn case_2() {
         assert_eq!(76576500, Euler::highly_divisible_triangular_number(500));
     }
+
+    // #[test]
+    // fn case_3() {
+    //     assert_eq!(842161320, Euler::highly_divisible_triangular_number(1000));
+    // }
+    //
+    // #[test]
+    // fn case_4() {
+    //     assert_eq!(842161320, Euler::highly_divisible_triangular_number(10000));
+    // }
 }
