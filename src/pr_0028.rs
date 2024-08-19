8eca226e544527a776101d7ceb24a967d3ebec69
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-08-19 16:01:49 +0100

    refactor: optimise 28

--- src/pr_0028.rs
+++ src/pr_0028.rs
@@ -3,50 +3,21 @@ pub struct Euler;
 const DIRS: &[(i32, i32); 4] = &[(0, -1), (1, 0), (0, 1), (-1, 0)];
 
 impl Euler {
-    pub fn number_spiral_diagonals(n: usize) -> usize {
+    pub const fn number_spiral_diagonals(n: usize) -> usize {
         assert!(n % 2 != 0);
-        let mut grid = vec![vec![0; n]; n];
-        grid[n / 2][n / 2] = 1;
-        let mut m = i32::try_from(n).unwrap();
-        let (mut curr, mut direction) = (n.pow(2), 0);
-        let mut i = 0;
-        let mut step = n - 1;
-        loop {
-            let ring = step * 4;
-            let (mut row, mut col) = (i, m - 1);
-            for _ in 0..ring {
-                grid[usize::try_from(row).unwrap()][usize::try_from(col).unwrap()] = curr;
-                row += DIRS[direction].0;
-                col += DIRS[direction].1;
-                if Self::is_border(m, i, row, col) {
-                    direction = (direction + 1) % 4;
-                }
-                curr -= 1;
+        let (mut result, mut i, mut m, mut round) = (1, 2, 2, 1);
+        while i <= n.pow(2) {
+            if round == 5 {
+                m += 2;
+                round = 1;
             }
-            if curr == 1 {
-                break;
+            if (i - 1) % m == 0 {
+                result += i;
+                round += 1;
             }
-            step -= 2;
-            m -= 1;
             i += 1;
         }
-        let mut result = 0;
-        let mut l = 0_i32;
-        let mut r = i32::try_from(n - 1).unwrap();
-        for row in grid {
-            result += row[usize::try_from(l).unwrap()];
-            result += row[usize::try_from(r).unwrap()];
-            l += 1;
-            r -= 1;
-        }
-        result - 1
-    }
-
-    const fn is_border(n: i32, i: i32, row: i32, col: i32) -> bool {
-        (row == i && col == i)
-            || (row == n - 1 && col == i)
-            || (row == n - 1 && col == n - 1)
-            || (row == i && col == n - 1)
+        result
     }
 }
 
