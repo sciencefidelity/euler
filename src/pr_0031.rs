286f08d8ea1051bdda2016da2280ccb0ec198a2f
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-08-20 11:57:14 +0100

    refactor: optimise 31

--- src/pr_0031.rs
+++ src/pr_0031.rs
@@ -2,24 +2,27 @@ use std::cmp::Ordering;
 
 pub struct Euler;
 
-const COINS: [i32; 8] = [1, 2, 5, 10, 20, 50, 100, 200];
-
 impl Euler {
-    pub fn coin_sums(target: i32) -> i32 {
-        let mut coin_sums = CoinSums::new(target);
+    pub fn coin_sums(candidates: &[i32], target: i32) -> i32 {
+        let mut coin_sums = CoinSums::new(candidates, target);
         coin_sums.backtrack(target, 0);
         coin_sums.count
     }
 }
 
-struct CoinSums {
+struct CoinSums<'a> {
+    candidates: &'a [i32],
     target: i32,
     count: i32,
 }
 
-impl CoinSums {
-    pub const fn new(target: i32) -> Self {
-        Self { target, count: 0 }
+impl<'a> CoinSums<'a> {
+    pub const fn new(candidates: &'a [i32], target: i32) -> Self {
+        Self {
+            candidates,
+            target,
+            count: 0,
+        }
     }
 
     pub fn backtrack(&mut self, remain: i32, start: usize) {
@@ -27,7 +30,7 @@ impl CoinSums {
             Ordering::Less => {}
             Ordering::Equal => self.count += 1,
             Ordering::Greater => {
-                for (i, coin) in COINS.iter().enumerate().skip(start) {
+                for (i, coin) in self.candidates.iter().enumerate().skip(start) {
                     self.backtrack(remain - coin, i);
                 }
             }
@@ -39,8 +42,10 @@ impl CoinSums {
 mod tests {
     use super::*;
 
+    const COINS: [i32; 8] = [1, 2, 5, 10, 20, 50, 100, 200];
+
     #[test]
     fn case_1() {
-        assert_eq!(Euler::coin_sums(200), 73682);
+        assert_eq!(Euler::coin_sums(&COINS, 200), 73_682);
     }
 }
